219dacce1be794fec67bab292f0377067b4a9f47
Kairatto <kairattoss@gmail.com>
2023-05-04 18:16:50 +0600

    Initial commit

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .permissions import IsAuthorOrAllowAny
 from .serializers import CommentSerializer, PostSerializer, CategorySerializer
@@ -11,6 +13,18 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     authentication_classes = [SessionAuthentication, ]
     permission_classes = [IsAuthorOrAllowAny, ]
+
+    @action(detail=True, methods=['get'])
+    def get_video(self, request, pk=None):
+        post = self.get_object()
+        video_url = post.video.url
+        return Response({'url': video_url})
+
+    @action(detail=True, methods=['get'])
+    def get_image(self, request, pk=None):
+        post = self.get_object()
+        image_url = post.image.url
+        return Response({'image_url': image_url})
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
